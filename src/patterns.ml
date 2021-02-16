36eccc921e2c4c3a5c137c3943d2ba7c56357165
Alban Gruin <alban.gruin@univ-tlse3.fr>
2021-02-16 11:51:28 +0100

    patterns: detect loads and stores
    Signed-off-by: Alban Gruin <alban.gruin@univ-tlse3.fr>

--- src/patterns.ml
+++ src/patterns.ml
@@ -7,6 +7,8 @@ type stats = {
   la : int;
   far_calls : int;
   li : int;
+  load : int;
+  store : int;
   load_global : int;
   store_global : int;
 }
@@ -20,12 +22,18 @@ let init_stats = {
   la = 0;
   far_calls = 0;
   li = 0;
+  load = 0;
+  store = 0;
   load_global = 0;
   store_global = 0;
 }
 
 let count_instr_patterns instrs =
   let rec aux st = function
+    | Decoder.(Itype ((Lb | Lh | Lw | Lbu | Lhu), _, _, _) :: rem) ->
+      aux {st with load = st.load + 1} rem
+    | Decoder.Stype _ :: rem ->
+      aux {st with store = st.store + 1} rem
     | Decoder.(Utype (Auipc, rs1, _) :: Itype (Addi, _, rs2, _) :: rem) when rs1 = rs2 ->
       aux {st with lla = st.lla + 1} rem
     | Decoder.(Utype (Auipc, rs1, _) :: Itype (Jalr, _, rs2, _) :: rem) when rs1 = rs2 ->
@@ -62,5 +70,7 @@ let find_patterns instrs =
   print_st st.la "la";
   print_st st.far_calls "far calls";
   print_st st.li "li";
+  print_st st.load "loads";
+  print_st st.store "stores";
   print_st st.load_global "load global";
   print_st st.store_global "store global"
